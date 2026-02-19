a8155c74177e158354472a6935a5c9261e45e424
Huyubin <1979578551@qq.com>
2026-02-19 15:41:12 +0800

    加了三个图表

--- pretty_trees.py
+++ pretty_trees.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.title('SF Trees')
 st.write('''This app analyses trees in San Francisco using a dataset kindly provided by SF DPW.''')
+
+trees_df = pd.read_csv('trees.csv')
+df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
+df_dbh_grouped.columns = ['tree_count']
+st.line_chart(df_dbh_grouped)
+st.bar_chart(df_dbh_grouped)
+st.area_chart(df_dbh_grouped)
 
 #让用户输入决定列宽度
 first_width = st.number_input('First Width',min_value=1,value=1)
