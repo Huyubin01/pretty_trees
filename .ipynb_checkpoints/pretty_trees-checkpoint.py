2a49d919045f7ea81f88900677eda5198f4a105d
Huyubin <1979578551@qq.com>
2026-02-19 18:48:20 +0800

    加了可编辑数据的页面

--- .ipynb_checkpoints/pretty_trees-checkpoint.py
+++ .ipynb_checkpoints/pretty_trees-checkpoint.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
 #设为宽格式
 st.set_page_config(layout = 'wide')
 
---
+++ pages/data_quality.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import streamlit as st
+
+st.title('SF Trees Data Quality App')
+st.write('''This app is a data quality tool for the SF trees dataset. Edit the data and save to a new file!''')
+
+trees_df = pd.read_csv('trees.csv')
+trees_df = trees_df.dropna(subset=['longitude','latitude'])
+trees_df_filtered = trees_df[trees_df['legal_status'] == 'Private']
+#可读
+#st.dataframe(trees_df)
+#可编辑
+edited_df = st.experimental_data_editor(trees_df_filtered)
